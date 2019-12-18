8052b318da90e1aeb9e2df5bfcc4f9b1e7b01be8
sudo apt update <charlottemontane@hotmail.com>
2019-12-18 16:10:45 -0500

    Add dotenv - Protect my secret data in .env file

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -12,16 +12,16 @@ class CocktailsController < ApplicationController
     end
 
     def create
-        @Cocktail = Cocktail.new(params[:Cocktail])
-        if @Cocktail.save
-          redirect_to @Cocktail
+        @cocktail = Cocktail.new(cocktail_params)
+        if @cocktail.save
+          redirect_to cocktail_path(@cocktail)
         else
           render "new"
         end  
     end
  private
     def cocktail_params
-        params.require(:cocktail).permit(:name)
+        params.require(:cocktail).permit(:name,:photo)
     end
 
 end
